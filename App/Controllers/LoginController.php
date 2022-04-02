763e236513e0a6772d7d8cdc4ffc45174cc27abc
K0n3k <nguyenhuu.kevin@gmail.com>
2022-04-02 16:27:55 +0200

    login

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -8,8 +8,8 @@ class LoginController extends Controller {
         if (!array_key_exists("parameters", $this->server)) {
             echo $this->twig->render('Login.twig');
         } else {
-            $login = new LoginModel();
-            dd($login);
+            $login = new LoginModel($this->server["parameters"]["username"], $this->server["parameters"]["password"]);
+            dd($this->server,$login);
         }
         
     }
