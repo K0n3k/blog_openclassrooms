d69e82d1e6ce281f7d5721053c80b47e1637dece
K0n3k <nguyenhuu.kevin@gmail.com>
2022-04-07 19:09:54 +0200

    Crud

--- App/Controllers/LoginController.php
+++ App/Controllers/LoginController.php
@@ -6,17 +6,30 @@ use App\Models\LoginModel;
 class LoginController extends Controller {
     public function render() {
         $errorLogin = false;
+        session_start();
         if (array_key_exists("parameters", $this->server)) {
             $login = new LoginModel();
-            $connect = $login->connectUser($this->server["parameters"]["username"], $this->server["parameters"]["password"]);
-            if(empty($connect)) {
-                $errorLogin = true;
-            } else {
-                
+            $user = $login->connectUser(
+                $this->server["parameters"]["username"],
+                $this->server["parameters"]["password"]);
+                if(empty($user)) {
+                    $errorLogin = true;
+                } else {
+                if(!isset($_SESSION["user"])) {                    
+                    $_SESSION["user"] = $user;
+                    
+                }
             }
         }
-        echo $this->twig->render('Login.twig', ["errorLogin" => $errorLogin]);
-        dd($connect,$errorLogin);
+        
+        if(isset($_SESSION["user"])) {
+            dd($_SESSION);
+        } else {
+            $_SESSION = array();
+            session_destroy();
+            echo $this->twig->render('Login.twig', ["errorLogin" => $errorLogin]);    
+        }
+        //echo $this->twig->render('Login.twig', ["errorLogin" => $errorLogin]);
         
     }
 }
